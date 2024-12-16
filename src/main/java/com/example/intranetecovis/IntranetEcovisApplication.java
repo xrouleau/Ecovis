641dfd3b5b81847b10b7e97c518e2ecb728ffeac
info1 <xrouleau01@cegepoutaouais.qc.ca>
2024-12-16 09:38:51 -0500

    Changements pour le déploiement

--- src/main/java/com/example/intranetecovis/IntranetEcovisApplication.java
+++ src/main/java/com/example/intranetecovis/IntranetEcovisApplication.java
@@ -2,9 +2,10 @@ package com.example.intranetecovis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IntranetEcovisApplication {
+public class IntranetEcovisApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(IntranetEcovisApplication.class, args);
